5f1d38420235c70026df21cf64c9bf20f571b3f5
Hajra <72098999+hajraladha@users.noreply.github.com>
2021-11-15 02:37:48 +0000

    Created for each loop and IIFE Wrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
-let pokemonList = [
+  //IIFE wrap
+  let pokemonRepository = (function () {
+  //Pokemon list
+  let pokemonList = [
   { name: "Pickachu", type: ["fairy"], weight: 6 },
   { name: "Jigglypuff", type: ["fairy"], weight: 5.5 },
   { name: "Butterfree", type: ["bug"], weight: 32 }
 ];
-for (let i = 0; i < pokemonList.length; i++) {
- document.write(pokemonList[i].name + " - weight: " + pokemonList[i].weight +"kg");
-let weight = '10';
-if (pokemonList[i].weight > 10) 
-  document.write(" Wow, that is heavy!" + "<br>");
-  else document.write("<br>");
-  //  block of code to be executed if the condition is true
+ return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+  })();
+  function myPokemonWrite(pokemon) {
+  document.write(pokemon.name + ' (weight: ' + pokemon.weight + ')');
+  if (pokemon.weight >= 10) {
+    document.write(' - Wow! That is heavy!');
 }
+pokemonRepository.add
+console.log(pokemonRepository.getAll());
+pokemonRepository.add ({name: 'Sharpedo', type: ['fish'], weight: 88.8 });;
+console.log(pokemonRepository.getAll());
