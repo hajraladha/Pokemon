e335b3714c28892ab7b724e97d99a7353a38a903
Hajra <72098999+hajraladha@users.noreply.github.com>
2021-11-19 01:40:30 +0000

    For Each Loop and IIFE wrap

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,32 @@
   { name: "Butterfree", type: ["bug"], weight: 32 }
 ];
  return {
-    add: function(pokemon) {
+    function add(pokemon) {
       pokemonList.push(pokemon);
-    },
-    getAll: function() {
-      return pokemonList;
     }
+    function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
   };
   })();
+
   function myPokemonWrite(pokemon) {
   document.write(pokemon.name + ' (weight: ' + pokemon.weight + ')');
   if (pokemon.weight >= 10) {
     document.write(' - Wow! That is heavy!');
-}
-pokemonRepository.add
-console.log(pokemonRepository.getAll());
+}}
+
 pokemonRepository.add ({name: 'Sharpedo', type: ['fish'], weight: 88.8 });;
 console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addpokemonList(pokemon);
+}
+
+
+
+
+  
